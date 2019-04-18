8072c8a624d0f914dc7709db49fbbc56db29ab6d
Daniel Varga <daniel.varga@prezi.com>
2019-04-18 16:25:12 +0200

    refactor, before-after perturbing clients.

--- bipartite_test.py
+++ bipartite_test.py
@@ -29,19 +29,12 @@ def distance_test():
             print(squaredDistances[i, j], np.linalg.norm(clients[i] - servers[j]) ** 2)
 
 
-def main():
-    n = 100
-    d = 10
-
-    np.random.seed(1)
-    clients = np.random.normal(size=(n, d))
-    servers = np.random.normal(size=(n, d)) + 1
-    distances = np.sqrt(pairwiseSquaredDistances(clients, servers))
+def distances_to_graph(distances, mode):
+    n = distances.shape[0]
     G = nx.Graph()
     for i in range(n):
         G.add_node(i, bipartite=0)
         G.add_node(n + i, bipartite=1)
-    mode = "sparse"
     if mode == "sparse":
         k = 20
         for i in range(n):
@@ -67,14 +60,27 @@ def main():
             randoms = np.random.choice(n, k)
             for j in randoms:
                 G.add_edge(i, n+j, weight=-distances[i, j])
+    return G
 
-    matching = nx.algorithms.matching.max_weight_matching(G, maxcardinality=True, weight='weight')
-    print(len(matching), "/", n)
+
+def weight_of_matching(matching, distances):
+    n = distances.shape[0]
+    totalDistance = 0.0
+    for a, b in matching:
+        if a >= n:
+            b, a = a, b
+        client = a
+        server = b - n
+        totalDistance += distances[client, server]
+    return totalDistance
+
+
+def plot(servers, clients, matching, filename):
+    n = len(servers)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(clients[:, 0], clients[:, 1], c='red')
     ax.scatter(servers[:, 0], servers[:, 1], c='blue')
-    totalDistance = 0.0
     for a, b in matching:
         if a >= n:
             b, a = a, b
@@ -84,10 +90,44 @@ def main():
         ys = [clients[client, 1], servers[server, 1]]
         l = Line2D(xs, ys)
         ax.add_line(l)
-        totalDistance += distances[client, server]
-    print(totalDistance)
+    ax.set_xlim(-3, +5)
+    ax.set_ylim(-3, +5)
+    plt.savefig(filename)
+    plt.close()
 
-    plt.savefig(sys.argv[1])
+
+def main():
+    n = 100
+    d = 2
+
+    np.random.seed(1)
+    clients = np.random.normal(size=(n, d))
+    servers = np.random.normal(size=(n, d)) + 1
+    distances = np.sqrt(pairwiseSquaredDistances(clients, servers))
+    G = distances_to_graph(distances, mode="dense")
+
+    matching = nx.algorithms.matching.max_weight_matching(G, maxcardinality=True, weight='weight')
+    print(len(matching), "/", n)
+
+    totalDistance = weight_of_matching(matching, distances)
+    print("before", totalDistance)
+
+    plot(servers, clients, matching, "before.png")
+
+    batchsize = 10
+    print("moving", batchsize, "clients")
+    # clients[:batchsize, :] = np.random.normal(size=(batchsize, d))
+    deviation = 0.1
+    clients[:batchsize, :] += np.random.normal(size=(batchsize, d)) * deviation
+
+    distances = np.sqrt(pairwiseSquaredDistances(clients, servers))
+    G = distances_to_graph(distances, mode="dense")
+    matching2 = nx.algorithms.matching.max_weight_matching(G, maxcardinality=True, weight='weight')
+    totalDistance = weight_of_matching(matching2, distances)
+    print("intersection", len(matching.intersection(matching2)), "/", len(matching))
+    print("after", totalDistance)
+
+    plot(servers, clients, matching2, "after.png")
 
 
 if __name__ == "__main__":
