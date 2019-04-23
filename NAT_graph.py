52004bd2eecc02afad20b65817970d992a194c87
ttossenb <43469325+ttossenb@users.noreply.github.com>
2019-04-23 17:00:41 +0200

    reworked graph creation; added optional random edges

--- NAT_graph.py
+++ NAT_graph.py
@@ -7,6 +7,7 @@ import time
 from sortedcontainers import SortedSet
 import directed_ES as ES
 from math import sqrt, log, floor
+from random import randint
 
 
 def normalize(a, order=2, axis=-1):
@@ -38,7 +39,7 @@ def buildServers(n, d):
     return targetPoints, B, targetIndex
 
 
-def createGraph(n, d, n_trees, n_nbrs):
+def createGraph(n, d, latentPoints, n_trees, n_nbrs, n_rndms):
     #n = number of traing inputs
     #n = 50000
     #d = latent dimension
@@ -62,9 +63,6 @@ def createGraph(n, d, n_trees, n_nbrs):
     targetPoints = np.reshape(((-2) * binaries + 1) * targetPoints, ((2 ** d) * k, d))
     np.random.shuffle(targetPoints)
 
-    #placeholder for input
-    latentPoints = normalize(np.random.normal(0, 1, (n, d)))
-
     #create AnnoyIndex in R^d
     targetIndex = AnnoyIndex(d)
     #add each of the target points
@@ -85,28 +83,42 @@ def createGraph(n, d, n_trees, n_nbrs):
     #find the closest neighbours for each latent point and their distances,
     #and also create the biadjacency sparse matrix of the desired bipartite graph
     #initialize with an empty sparse matrix
-    B = coo_matrix((latentPoints.shape[0], targetPoints.shape[0]), dtype=np.float16)
+    #B = coo_matrix((latentPoints.shape[0], targetPoints.shape[0]), dtype=np.float16)
     #initialize closeIndices and closeDistances
     closeIndices = np.zeros((latentPoints.shape[0], n_nbrs), dtype=int)
     closeDistances = np.zeros((latentPoints.shape[0], n_nbrs), dtype=np.float16)
 
+    #create the bipartite graph in networkx
+    G = nx.Graph()
+    G.add_nodes_from(range(0, latentPoints.shape[0]), bipartite=0)
+    G.add_nodes_from(range(latentPoints.shape[0], latentPoints.shape[0] + targetPoints.shape[0]), bipartite=1)
+
     for i in range(latentPoints.shape[0]):
-        tempTuple = loadedIndex.get_nns_by_vector(latentPoints[i], n_nbrs, include_distances=True)
-        closeIndices[i] = np.array(tempTuple[0])
-        closeDistances[i] = np.array(tempTuple[1])
+        (closeIndices[i], closeDistances[i]) = loadedIndex.get_nns_by_vector(latentPoints[i], n_nbrs, include_distances=True)
+        for j in range(n_nbrs):
+            G.add_edge(i, latentPoints.shape[0] + closeIndices[i, j], weight=closeDistances[i, j])
+        for j in range(n_rndms):
+            tempRandInt = randint(0, targetPoints.shape[0] - 1)
+            G.add_edge(i, latentPoints.shape[0] + tempRandInt,
+                       weight=np.linalg.norm(targetPoints[tempRandInt] - latentPoints[i])) #might add the same edge twice
 
     #end2 = time.clock()
     #start3 = time.clock()
 
     #create the biadjacency matrix
     #arg1=data=(data, (rows, cols))
-    B = coo_matrix((closeDistances.flatten(), (np.repeat(np.arange(latentPoints.shape[0]), n_nbrs), closeIndices.flatten())))
+    #B = coo_matrix((closeDistances.flatten(), (np.repeat(np.arange(latentPoints.shape[0]), n_nbrs), closeIndices.flatten())))
 
     #create the bipartite graph in networkx
-    G = bipartite.from_biadjacency_matrix(B)
+    #G = bipartite.from_biadjacency_matrix(B)
+    #G.add_nodes_from(range(n, n + (2 ** d) * k), )
+    print(len(set(G)))
 
-    client_nodes = SortedSet({n for n, d in G.nodes(data=True) if d['bipartite']==0}) #0 .. n-1
-    server_nodes = SortedSet(set(G) - client_nodes)
+    client_nodes = SortedSet(range(latentPoints.shape[0]))
+    server_nodes = SortedSet(range(latentPoints.shape[0], latentPoints.shape[0] + targetPoints.shape[0]))
+
+    #client_nodes = SortedSet({n for n, d in G.nodes(data=True) if d['bipartite']==0}) #0 .. n-1
+    #server_nodes = SortedSet(set(G) - client_nodes)
 
     #end3 = time.clock()
 
@@ -114,7 +126,7 @@ def createGraph(n, d, n_trees, n_nbrs):
     #print("Elapsed time during the initialization of the LSH-forest: ", end2-start2)
     #print("Elapsed time during the creation of the bipartite graph: ", end3-start3)
 
-    return G, client_nodes, server_nodes, loadedIndex
+    return G, client_nodes, server_nodes, loadedIndex, targetPoints
 
 
 def createESGraph(G, H, M, client_nodes, server_nodes, max_level, source_node=-1):
@@ -148,7 +160,7 @@ def deleteBatchOfClients(clients_to_delete, H, M, max_level, source_node=-1):
     ES.deleteClients(clients_to_delete, H, M, source_node, max_level)
 
 
-def updateBatch(G, n, annoy_index, batch_indices, latentBatch, H, M, max_level, n_nbrs, source_node=-1):
+def updateBatch(G, n, annoy_index, batch_indices, latentBatch, targetPoints, H, M, max_level, n_nbrs, n_rndms=0, source_node=-1):
     #latentBatch=latentPoints["batch_indices as np.array"] (must be sorted)
     #delete the nodes (and the edges from these nodes)
     deleteBatchOfClients(batch_indices, H, M, max_level)
@@ -162,18 +174,29 @@ def updateBatch(G, n, annoy_index, batch_indices, latentBatch, H, M, max_level, 
     for i in range(len(batch_indices)):
         (closeIndices[i], closeDistances[i]) = annoy_index.get_nns_by_vector(latentBatch[i], n_nbrs, include_distances=True)
         for j in range(n_nbrs):
-            G.add_edge(closeIndices[i][j] + n, batch_indices[i], weight=closeDistances[i][j])
+            G.add_edge(batch_indices[i], closeIndices[i][j] + n, weight=closeDistances[i][j])
+        for j in range(n_rndms):
+            tempRandInt = randint(0, n-1) #n as number of SERVER nodes, might need to change it!!!
+            G.add_edge(batch_indices[i], n + tempRandInt,
+                       weight=np.linalg.norm(latentBatch[i] - targetPoints[tempRandInt])) #might add the same edge twice
     ES.addClients(G, batch_indices, H, M, source_node, max_level)
 
 
 def main():
-    n = 50000
+    n = 49152
     #max_level = floor(sqrt(n) * sqrt(log(n))) #=735 for n=50000
-    max_level = 7
+    max_level = 8
+    n_nbrs = 10
+    n_rndms = 0
+
+    #placeholder for input
+    #latentPoints = normalize(np.random.normal(0, 1, (n, d)))
+    latentPoints = normalize(np.load('latent_points_10.npy')[:n])
 
     start1 = time.clock()
     #---before training---
-    (G, client_nodes, server_nodes, annoy_index) = createGraph(n=n, d=10, n_trees=60, n_nbrs=10)
+    (G, client_nodes, server_nodes, annoy_index, targetPoints) = createGraph(n=n, d=10, latentPoints=latentPoints,
+                                                               n_trees=60, n_nbrs=n_nbrs, n_rndms=n_rndms)
     end1 = time.clock()
     print('Created G bipartate graph. Elapsed time: ', end1 - start1)
 
@@ -181,6 +204,8 @@ def main():
     H = nx.DiGraph()
     #initialize M digraph of the matching directed from C to S
     M = nx.DiGraph()
+    M.add_nodes_from(client_nodes)
+    M.add_nodes_from(server_nodes)
 
     start2 = time.clock()
     createESGraph(G, H, M, client_nodes, server_nodes, max_level)
@@ -191,12 +216,12 @@ def main():
     start3 = time.clock()
     batch_indices = SortedSet(range(0, 200)) #placeholder
     latentBatch = normalize(np.random.normal(0, 1, size=(200, 10))) #placeholder
-    updateBatch(G, n, annoy_index, batch_indices, latentBatch, H, M, max_level, n_nbrs=10)
+    updateBatch(G, n, annoy_index, batch_indices, latentBatch, targetPoints, H, M, max_level, n_nbrs=n_nbrs, n_rndms=n_rndms)
     end3 = time.clock()
     print('Modified on one batch. Elapsed time: ', end3 - start3)
     #Todo modify the loss function with M
 
-    print('number of matching edges: ', len(M.edges()))
+    print('number of matching edges / n : ', len(M.edges()) / n)
 
 if __name__ == "__main__":
     main()
