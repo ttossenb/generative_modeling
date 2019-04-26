e59d1be51846174eb0613ca864a01fc76015de5d
ttossenb <43469325+ttossenb@users.noreply.github.com>
2019-04-26 11:21:02 +0200

    rebuilding M with each epoch

--- NAT_graph.py
+++ NAT_graph.py
@@ -155,6 +155,25 @@ def createESGraph(G, H, M, client_nodes, server_nodes, max_level, source_node=-1
     ES.addClients(G, clients_to_add, H, M, source_node, max_level)
 
 
+def initializeESGraph(H, client_nodes, server_nodes, source_node=-1):
+    #H = directed ES graph
+    H.add_node(source_node, level=0)
+    H.add_nodes_from(client_nodes, level=1)
+    H.add_nodes_from(server_nodes, level=1)
+
+    #connect the source node to the server nodes
+    for s in server_nodes:
+        H.add_edge(source_node, s)
+        H.nodes[s]['witnesses'] = SortedSet()
+        H.nodes[s]['witnesses'].add(source_node)
+
+    #connect the source node to the client nodes
+    for c in client_nodes:
+        H.add_edge(source_node, c)
+        H.nodes[c]['witnesses'] = SortedSet()
+        H.nodes[c]['witnesses'].add(source_node)
+
+
 def deleteBatchOfClients(clients_to_delete, H, M, max_level, source_node=-1):
     #clients_to_delete = sorted set of the indices of the batch elements to delete
     ES.deleteClients(clients_to_delete, H, M, source_node, max_level)
@@ -182,6 +201,33 @@ def updateBatch(G, n, annoy_index, batch_indices, latentBatch, targetPoints, H, 
     ES.addClients(G, batch_indices, H, M, source_node, max_level)
 
 
+def addBatch(G, n, annoy_index, batch_indices, latentBatch, targetPoints, H, M, F, max_level, n_nbrs, n_rndms=0, source_node=-1):
+    #latentBatch=latentPoints["batch_indices as np.array"] (must be sorted)
+    #delete the nodes (and the edges from these nodes)
+    G.remove_nodes_from(batch_indices)
+    #add back the nodes
+    G.add_nodes_from(batch_indices)
+    #initialize closeIndices and closeDistances
+    closeIndices = np.zeros((latentBatch.shape[0], n_nbrs), dtype=int)
+    closeDistances = np.zeros((latentBatch.shape[0], n_nbrs), dtype=np.float16)
+    #add new weighted edges to G
+    for i in range(len(batch_indices)):
+        (closeIndices[i], closeDistances[i]) = annoy_index.get_nns_by_vector(latentBatch[i], n_nbrs, include_distances=True)
+        for j in range(n_nbrs):
+            G.add_edge(batch_indices[i], closeIndices[i][j] + n, weight=closeDistances[i][j])
+        for j in range(n_rndms):
+            tempRandInt = randint(0, n-1) #n as number of SERVER nodes, might need to change it!!!
+            G.add_edge(batch_indices[i], n + tempRandInt,
+                       weight=np.linalg.norm(latentBatch[i] - targetPoints[tempRandInt])) #might add the same edge twice
+    ES.addClients(G, batch_indices, H, M, source_node, max_level)
+    #readjust F
+    for c in batch_indices:
+        for s in set(F.successors(c)):
+            F.remove_edge(c, s)
+        for s in M.successors(c):
+            F.add_edge(c, s)
+
+
 def main():
     n = 49152
     #max_level = floor(sqrt(n) * sqrt(log(n))) #=735 for n=50000
@@ -206,22 +252,35 @@ def main():
     M = nx.DiGraph()
     M.add_nodes_from(client_nodes)
     M.add_nodes_from(server_nodes)
+    #initialize F bipartate graph of forces
+    F = nx.DiGraph()
 
     start2 = time.clock()
     createESGraph(G, H, M, client_nodes, server_nodes, max_level)
     end2 = time.clock()
+
+    print('number of matching edges / n : ', len(M.edges()) / n)
+
+    #deepcopy M to F
+    F.add_nodes_from(M.nodes)
+    F.add_edges_from(M.edges)
     print('Created the initial ES graph. Elapsed time: ', end2 - start2)
+
+    #---at the start of each epoch---
+    G.clear()
+    H.clear()
+    initializeESGraph(H, client_nodes, server_nodes, source_node=-1)
+    M.clear()
 
     #---after training each batch---
     start3 = time.clock()
     batch_indices = SortedSet(range(0, 200)) #placeholder
     latentBatch = normalize(np.random.normal(0, 1, size=(200, 10))) #placeholder
-    updateBatch(G, n, annoy_index, batch_indices, latentBatch, targetPoints, H, M, max_level, n_nbrs=n_nbrs, n_rndms=n_rndms)
+    addBatch(G, n, annoy_index, batch_indices, latentBatch, targetPoints, H, M, F, max_level, n_nbrs=n_nbrs, n_rndms=n_rndms)
     end3 = time.clock()
     print('Modified on one batch. Elapsed time: ', end3 - start3)
     #Todo modify the loss function with M
 
-    print('number of matching edges / n : ', len(M.edges()) / n)
 
 if __name__ == "__main__":
     main()
